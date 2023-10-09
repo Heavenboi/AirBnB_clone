df943a4e3cceedd0b84f6fdf9002215b5047a2d2
Heavenboi <nigelzenda98@gmail.com>
2023-10-09 23:39:37 +0200

    added some more methods on the BaseModel file

--- models/base_model.py
+++ models/base_model.py
@@ -1,6 +1,23 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
+from datetime import datetime 
+import uuid
 
 class BaseModel:
     '''class that defines all the common attributes/methods for other classes'''
     def __init__(self):
-        pass
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = self.created_at
+
+    def updated(self):
+        self.created_at = datetime.now()
+
+    def save(self):
+        self.updated()
+
+    def __str__(self):
+
+
+
+if __name__ == '__main__':
+    my_model = BaseModel
---
+++ models/test_base_model.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+from base_model import BaseModel
+
+my_model = BaseModel()
+my_model.name = "My First Model"
+my_model.my_number = 89
+
+print(my_model.name)
+print(my_model.my_number)
+print(my_model.id)
