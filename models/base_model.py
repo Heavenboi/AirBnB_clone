84a679c12e6bebf825db1b70d75d471ec9cd35a5
Heavenboi <nigelzenda98@gmail.com>
2023-10-10 12:44:38 +0200

    udated basemodel

--- models/base_model.py
+++ models/base_model.py
@@ -11,12 +11,17 @@ class BaseModel:
         self.updated_at = datetime.now()
 
     def save(self):
-        self.update_at.now()
+        self.update_at = datetime.now()
 
     def __str__(self):
         return "[{}] ({}){}".format(self.__class__.__name__, self.id, self.__dict__)
+    def to_dict(self):
+        ''' returns a dictionary containing all keys/value of __dict__ of instance '''
+        instance_dict = self.__dict__.copy()
+        instance_dict['__class__'] = self.__class__.__name__
 
+        instance_dict['created_at'] = self.created_at.isoformat()
+        instance_dict['updated_at'] = self.updated_at.isoformat()
 
+        return instance_dict
 
-if __name__ == '__main__':
-    my_model = BaseModel
