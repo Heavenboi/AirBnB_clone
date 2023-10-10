653abe8f461c0b9f0267381dfc17fbb6bb070ab7
Heavenboi <nigelzenda98@gmail.com>
2023-10-10 11:32:46 +0200

    updateded the BaseModel

--- models/base_model.py
+++ models/base_model.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+import models
 from datetime import datetime 
 import uuid
 
@@ -7,15 +8,13 @@ class BaseModel:
     def __init__(self):
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
-        self.updated_at = self.created_at
-
-    def updated(self):
-        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
 
     def save(self):
-        self.updated()
+        self.update_at.now()
 
     def __str__(self):
+        return "[{}] ({}){}".format(self.__class__.__name__, self.id, self.__dict__)
 
 
 
