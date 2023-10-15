e472b69ab16ef737959490be05cbe84f51efa8f6
the1Riddle <elvisotienomboya@gmail.com>
2023-10-15 14:31:29 +0300

    merged models to main

--- models/base_model.py
+++ models/base_model.py
@@ -4,6 +4,7 @@
 import uuid
 from datetime import datetime
 import models
+from models.engine import file_storage
 
 
 class BaseModel:
--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,10 +1,25 @@
-#!/usr/bin/env python3
-"""
-    Define class FileStorage Module
-"""
+#!/usr/bin/python3
+
 import json
 import models
 from models.base_model import BaseModel
+from models.city import City
+from models.user import User
+from models.place import Place
+from models.amenity import Amenity
+from models.state import State
+from models.review import Review
+
+
+class_names = {
+    "BaseModel": BaseModel,
+    "City": City,
+    "User": User,
+    "Place": Place,
+    "Amenity": Amenity,
+    "State": State,
+    "Review": Review
+}
 
 
 class FileStorage:
@@ -23,12 +38,12 @@ class FileStorage:
 
     def save(self):
         """Serializes __objects to the JSON file."""
-        serializd = {key: obj.to_dict() for key, obj in self.__objects.items()}
+        serialized = {key: obj.to_dict() for key,
+                      obj in self.__objects.items()}
         with open(self.__file_path, "w") as file:
-            json.dump(serializd, file)
+            json.dump(serialized, file)
 
     def reload(self):
-        """some comments"""
         try:
             with open(FileStorage.__file_path, 'r') as file:
                 content = file.read()
@@ -38,10 +53,10 @@ class FileStorage:
                 for key, value in objects_dict.items():
                     class_name, obj_id = key.split(".")
                     obj_key = f"{class_name}.{obj_id}"
-                    if obj_key not in FileStorage.__objects:
-                        class_definition = globals().get(class_name)
-                        if class_definition:
-                            obj = class_definition(**value)
-                            FileStorage.__objects[obj_key] = obj
+                if obj_key not in FileStorage.__objects:
+                    class_definition = globals().get(class_name)
+                    if class_definition:
+                        obj = class_definition(**value)
+                        FileStorage.__objects[obj_key] = obj
         except FileNotFoundError:
             pass
