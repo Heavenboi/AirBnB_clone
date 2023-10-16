69e6a1d08a121c5fc58bc481ec958d902c0ad3cf
Heavenboi <nigelzenda98@gmail.com>
2023-10-17 00:19:36 +0200

    fixed some errors on the file_storage.py

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -2,25 +2,6 @@
 
 from datetime import datetime
 import json
-import models
-from models.base_model import BaseModel
-from models.user import User
-from models.city import City
-from models.place import Place
-from models.amenity import Amenity
-from models.state import State
-from models.review import Review
-
-class_names = {
-    "BaseModel": BaseModel,
-    "User": User,
-    "State": State,
-    "City": City,
-    "Amenity": Amenity,
-    "Place": Place,
-    "Review": Review
-    }
-
 
 class FileStorage:
     ''' Private class attribute '''
